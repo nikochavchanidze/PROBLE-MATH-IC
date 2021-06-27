f9a91cc97cd90302595de910fc82c1d1df03b275
natalia711 <84464536+natalia711@users.noreply.github.com>
2021-06-27 13:46:35 +0400

    Add files via upload

---
+++ fizika.js
@@ -0,0 +1,7 @@
+function simkvrive(){
+    var x = document.getelementbyid(masa);
+    var y = document.getelementbyid(moculoba);
+    var pasuxisveli = document.getelementbyid(pasuxi);
+    var simkvrive = x/y;
+    pasuxisveli.innerHTML = simkvrive;
+}
