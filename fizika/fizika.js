80dcdc78e7d0c2404140cafdec2ffa5c44d751f7
nikochavchanidze <chavniko@gmail.com>
2021-06-28 09:05:45 +0400

    .

---
+++ fizika/fizika.js
@@ -0,0 +1,6 @@
+function simkvrivisgamotvla() {
+    var x = document.getelementbyid(masa).value;
+    var y = document.getelementbyid(moculoba).value;
+    var simkvrive = (x/y);
+    document.getElementById('pasuxi').innerHTML = simkvrive;
+}
