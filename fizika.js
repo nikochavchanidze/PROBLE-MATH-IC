ebb2e10d2cf1fc01ed3a55a8f3fbb676587244b2
nikochavchanidze <84462710+nikochavchanidze@users.noreply.github.com>
2021-06-28 09:10:28 +0400

    Delete fizika.js

--- fizika.js
+++
@@ -1,7 +0,0 @@
-function simkvrive(){
-    var x = document.getelementbyid(masa);
-    var y = document.getelementbyid(moculoba);
-    var pasuxisveli = document.getelementbyid(pasuxi);
-    var simkvrive = x/y;
-    pasuxisveli.innerHTML = simkvrive;
-}
