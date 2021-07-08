92ea0e8ce38791b35b15e14c2123f2ab65227b94
nikochavchanidze <chavniko@gmail.com>
2021-07-08 20:51:34 +0400

    .

--- fizika/fizika.js
+++ fizika/fizika.js
@@ -4,3 +4,21 @@ var y = document.getElementById('moculoba').value;
 var simkvrive = (x/y);
 document.getElementById('pasuxi').innerHTML = simkvrive;
 }
+function wnevisgamotvla(){
+    var x=document.getElementById('simkvrive').value;
+    var y=document.getElementById('simagle').value;
+    var wneva = (x*9.8*y);
+    document.getElementById('pasuxiA').innerHTML=wneva;
+}
+function wneva(){
+    var x=document.getElementById('simdzimisdzala').value;
+    var y=document.getElementById('fartobi').value;
+    var wnev=x/y;
+    document.getElementById('pasuxiB').innerHTML=wnev;
+}
+function sichkare(){
+    var x =document.getElementById('mandzili').value;
+    var y =document.getElementById('dro').value;
+    var sichqare = x/y;
+    document.getElementById('pasuxiC').innerHTML=sichqare;
+}
