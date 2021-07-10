5d70a79fcee9b75bbf7803e5e2c885ce74c92d1c
nikochavchanidze <chavniko@gmail.com>
2021-07-11 00:02:17 +0400

    .

--- matematika/matematika.js
+++ matematika/matematika.js
@@ -7,3 +7,10 @@ function dafesveba(){
     var ricxvif = document.getElementById('ricxvif').value;
     document.getElementById('pasuxisadgilif').innerHTML = Math.sqrt( ricxvif );
 }
+function pitagora(){
+    var kateti1 = document.getElementById('kateti1').value;
+    var kateti2 = document.getElementById('kateti2').value;
+    var hipkvad = (kateti1*kateti1) + (kateti2*kateti2);
+    var hip = Math.sqrt( hipkvad );
+    document.getElementById('pasuxisadgilip').innerHTML = hip;
+}
