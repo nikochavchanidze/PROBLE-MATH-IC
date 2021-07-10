6bb5a30050214130ce6f01f9f8dfc208a7362515
nikochavchanidze <chavniko@gmail.com>
2021-07-10 18:24:16 +0400

    .

--- matematika/matematika.js
+++ matematika/matematika.js
@@ -3,3 +3,7 @@ function akvadrateba() {
     var xarisx = document.getElementById('xarisxi').value;
     document.getElementById('pasuxisadgili').innerHTML = a**xarisx;
 }
+function dafesveba(){
+    var ricxvif = document.getElementById('ricxvif').value;
+    document.getElementById('pasuxisadgilif').innerHTML = Math.sqrt( ricxvif );
+}
