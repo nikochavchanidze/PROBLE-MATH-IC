e2078fa990bc7cad95bf056d36eb2d563aafacd5
nikochavchanidze <chavniko@gmail.com>
2021-07-14 20:07:19 +0400

    .

--- ბიოლოგია/biologia.js
+++ ბიოლოგია/biologia.js
@@ -18,4 +18,27 @@ function simaglisgamotvlaq() {
     var shq = sh - 5;
     document.getElementById('simagleq').innerHTML = shq;
 }
+function bavshvisTmisFeri() { 
+    var dedis = document.getElementById('mom').value;
+    var mamis = document.getElementById('dad').value;
 
+    if((dedis == 'cisq') && (mamis == 'cisk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 99% ცისფერი, 1% მწვანე')
+    }else if((dedis == 'cisq') && (mamis == 'mwvk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 50% მწვანე, 50% ცისფერი')
+    }else if((dedis == 'mwvq') && (mamis == 'mwvk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 75% მწვანე, 25% ცისფერი')
+    }else if((dedis == 'mwvq') && (mamis == 'yavk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 35% მწვანე, 15% ცისფერი, 50% ყავისფერი')
+    }else if((dedis == 'yavq') && (mamis == 'cisk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 50% ყავისფერი, 50% ცისფერი')
+    }else if((dedis == 'mwvq') && (mamis == 'cisk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 99% ცისფერი, 1% მწვანე')
+    }else if((dedis == 'yavq') && (mamis == 'mwvk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 35% მწვანე, 15% ცისფერი, 50% ყავისფერი')
+    }else if((dedis == 'cisq') && (mamis == 'yavk')){
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 50% ყავისფერი, 50% ცისფერი')
+    }else{
+        document.getElementById('baganastvalisferi').innerHTML=(' ბავშვის თვალის ფერი იქნება 75% ყავისფერი, 20% მწვანე, 5% ცისფერი')
+    }
+}  
